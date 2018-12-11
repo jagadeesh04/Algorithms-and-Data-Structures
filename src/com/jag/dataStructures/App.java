4e873a6fb6811d3d52be7fbfc7431c461a1d924f
Jag Gangishetti <jagadeesh.g07@gmail.com>
2018-12-11 18:56:55 -0500

    Testing stack

---
+++ src/com/jag/dataStructures/App.java
@@ -0,0 +1,21 @@
+package com.jag.dataStructures;
+
+public class App {
+
+	public static void main(String[] args) {
+		Stack theStack = new Stack(10);
+		
+		theStack.push(20);
+		theStack.push(30);
+		theStack.push(40);
+		theStack.push(50);
+		theStack.push(60);
+		
+		while(!theStack.isEmpty()){
+			long value = theStack.pop();
+			System.out.println(value);
+		}
+		
+	}
+	
+}
